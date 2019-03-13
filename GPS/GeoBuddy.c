de6d6b72e37f45718ea676312d4c512e4a4e0ad4
Ian Yi Sui <ysui@usc.edu>
2019-03-13 15:15:18 -0700

    finished wiring the LCD

---
+++ GPS/GeoBuddy.c
@@ -0,0 +1,83 @@
+/*Inclusions*/
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <avr/interrupt.h>
+#include <avr/eeprom.h>
+#include "Adafruit_GPS_project.h"
+#include "serial.h"
+
+int main(void)
+{
+	char input; 
+	char temp[100];
+	char output_buf[256]; 
+
+	char longitude_str[20];
+	char latitude_str[20];
+		
+	int input_len;
+	// Initialize the serial and gps interface
+	serial_init();
+	gps_common_init();
+
+	//From here, we just want to continuously poll the gps for data and try to display that data
+	int count = 0;
+	while(1)
+	{
+		_delay_ms(1);
+				
+		/************* use the following block to print out raw data from the gps module */
+
+		// input = serial_in(); 
+		// serial_out(input);
+
+		/*************************************************************** **********/
+
+
+		/***** use the following block to print out gps data as a raw string */
+		//For testing, we will send this string out to the console anyway to check we are getting the right data in the main loop
+		
+		//strcpy(output_buf, "Raw GPS Data: ");
+		//strcpy(output_buf, input);
+		//input_len = strlen(output_buf);
+		//output_buf[input_len] = input;
+		//output_buf[input_len + 1] = '\r';
+		//output_buf[input_len + 2] = '\n';
+		//output_buf[input_len + 3] = '\0';
+		//snprintf(output_buf, sizeof(output_buf), "Raw GPS Data: %s\n", input);
+		//serial_string_out(output_buf);
+		//memset(output_buf, 0, sizeof output_buf);
+
+		/**************************************************************************/
+		
+		
+		input = gps_read();
+		count++;
+
+		//Check if the data is new (Uncomment as testing proceeds)
+		if(gps_newNMEAreceived() == true)
+		{
+			gps_parse(gps_lastNMEA()); 
+		}
+
+
+		//****************  output parsed GPS data  ******/
+		if(count > 300){
+			
+			dtostrf(latitude, 5, 5, latitude_str);			
+			dtostrf(longitude, 5, 5, longitude_str);
+
+			snprintf(output_buf, sizeof(output_buf), "Date: Day: %u, Month: %u, Year: %u, \r\n"
+							"Time: %u:%u:%u\r\nLocation: %s%c, %s%c, Fix: %d\r\n"
+							", Fix quality: %u, Num Satellites: %u\r\n", day, month, year, hour, minute, 
+							seconds, latitude_str, lat, longitude_str, lon, fix, fixquality, satellites);
+			input_len = strlen(output_buf);
+			serial_string_out(output_buf);
+			memset(output_buf, 0, sizeof output_buf);
+			count = 0;
+		}
+
+	}
+}
--- GPS/serial.c
+++ GPS/serial.c
@@ -1,10 +1,3 @@
-//simple testing program for sending and reading a char through the serial port
-
-//just a reminder for myself(Ian):
-//use 
-//sudo picocom --baud 9600 /dev/ttyUSB0 
-//to communicate with the serial port
-
 #include <avr/io.h>
 #include <string.h>
 #include <util/delay.h>
@@ -50,33 +43,4 @@ char serial_in ()
 	//Potential problem, could lock the program 
 	while ( !( UCSR0A & (1 << RXC0 )) );
 	return UDR0 ;
-}
-
-int main(void)
-{
-	char input; 
-    //set the max output line length to 80
-	char output_buf[80]; 
-	int input_len;
-	// Initialize the serial interface
-	serial_init();
-	while(1)
-	{
-		_delay_ms(1);
-		//read data from the gps module to the microcontroller
-		//and output it
-		input = serial_in();
-		serial_out(input);
-        
-		//strcpy(output_buf, "GPS data: ");
-		//input_len = strlen(output_buf);
-		//output_buf[input_len] = input;
-		//output_buf[input_len + 1] = '\r';
-		//output_buf[input_len + 2] = '\n';
-		//output_buf[input_len + 3] = '\0';
-		//serial_string_out(output_buf);
-		
-	
-		//memset(output_buf, 0, sizeof output_buf);
-	}
 }
