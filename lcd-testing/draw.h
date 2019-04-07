d3c031cabe7448eaba23e974f94dc1711ee92415
Ling Ye <lingye@usc.edu>
2019-04-06 22:50:43 -0700

    Update draw.h

--- lcd-testing/draw.h
+++ lcd-testing/draw.h
@@ -28,9 +28,9 @@ GFXfont  *gfxFont;
 void drawChar(int16_t x, int16_t y, unsigned char c, uint16_t color, uint8_t size);
 void drawString(unsigned char* str, int size, int16_t x, int16_t y, uint16_t color);
 
-void flood(uint16_t color, uint32_t len)
-void drawVLine(int16_t x, int16_t y, int16_t length,uint16_t color)
-void drawHLine(int16_t x, int16_t y, int16_t length, uint16_t color)
+void flood(uint16_t color, uint32_t len);
+void drawVLine(int16_t x, int16_t y, int16_t length,uint16_t color);
+void drawHLine(int16_t x, int16_t y, int16_t length, uint16_t color);
  
 void color_bars();
 void boxes();
