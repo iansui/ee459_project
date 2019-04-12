72843152efb26f381a879e2e2e6642115a5df0f2
Ian Yi Sui <ysui@usc.edu>
2019-04-11 18:14:30 -0700

    work on touch

--- ian/GeoBuddy.c
+++ ian/GeoBuddy.c
@@ -12,12 +12,12 @@
 #include <avr/interrupt.h>
 #include <avr/eeprom.h>
 
-#include "gps.h"
+// #include "gps.h"
 #include "serial.h"
 #include "lcd.h"
 #include "GeoBuddy.h"
 #include "i2c.h"
-
+/*
 void update_user_location(){
 
 	//update current user location
@@ -191,6 +191,7 @@ void drawGPS(){
 		}
 	}
 }
+*/
 
 int main(void){
 
@@ -212,11 +213,23 @@ int main(void){
 	gps_common_init();
 	int gps_iteration_count = 0;
 
+	//draw background
+	draw_box(0, 0, LCD_Width-1, LCD_Height-1, background_color);
+
 	// initialize i2c port
 	i2c_init(BDIV);
+	uint8_t threshold = 128;
+	uint8_t status;
+	status = i2c_io(TOUCH_DEVICE_ADDR, &threshold, 1, NULL, 0, NULL, 0);
+	if(status == 0){
+		drawChar(12, 12, 'N', text_color, 1);
+	}
+	else{
+		drawChar(12, 12, 'N', text_color, 1);
 
-	//draw background
-	draw_box(0, 0, LCD_Width-1, LCD_Height-1, background_color_test);
+	}
+
+
 
 	//set up testing data
 	goal_lat = 34.020506;
@@ -224,9 +237,44 @@ int main(void){
 	strcpy(goal_title, "Viterbi E-quad Fountain");
 	strncpy(goal_info, "This is a test message for drawParagraph(). If there is enough memory space, we can use this function to display some short info of the goal location. The max length is 6 lines with 36 char in each line", sizeof(goal_info));
 
+	
 	//start infinite loop
 	while(1){
 
+		//test i2c
+		uint8_t x = 30;
+
+		draw_box(0, 0, 50, 50, background_color);
+
+		uint8_t temp = TOUCH_REG_NUMTOUCHES;
+
+		status = i2c_io(TOUCH_DEVICE_ADDR, &temp, 1, NULL, 0, &x, 1);
+		if(status == 0){
+			drawChar(12, 12, 'Y', text_color, 1);
+		}	
+		else{
+			drawChar(12, 12, 'N', text_color, 1);
+
+		}
+
+		if(x != 30){
+			drawChar(48, 48, 'Y', text_color, 1);
+			
+			if(x == 0){
+				drawChar(10, 10, '0', text_color, 1);
+			}
+			else{
+				drawChar(10, 10, '1', text_color, 1);
+			}
+		}	
+		else{
+			drawChar(48, 48, 'N', text_color, 1);
+
+		}
+
+		_delay_ms(1000);
+
+/*
 		_delay_ms(1);
 			
 		//update user location
@@ -245,7 +293,10 @@ int main(void){
 			update_distance(goal_lat, goal_long);
 			gps_iteration_count = 0;
 			drawGPS();
+
+			
 		}
+*/
 
 	}
 }
--- ian/i2c.h
+++ ian/i2c.h
@@ -3,10 +3,19 @@
 #define I2C_H
 
 #include <avr/io.h>
+#include <stddef.h>
 
-# define FOSC 9830400
-# define BDIV ( FOSC / 100000 - 16) / 2 + 1
+#define FOSC 9830400
+#define BDIV ( FOSC / 100000 - 16) / 2 + 1
+
+#define TOUCH_DEVICE_ADDR 0x70
+#define TOUCH_REG_NUMTOUCHES 0x02
+#define FT62XX_REG_VENDID 0xA8
+#define FT62XX_DEFAULT_THRESHOLD 128
 
 void i2c_init(uint8_t bdiv);
+uint8_t i2c_io(uint8_t device_addr, uint8_t *ap, uint16_t an, 
+               uint8_t *wp, uint16_t wn, uint8_t *rp, uint16_t rn);
+
 
 #endif
